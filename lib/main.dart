7b7a6909b2ba841d9c9102c94470552df32e91bb
Sonats <ferreirafabio51@gmail.com>
2025-02-07 16:57:09 -0300

    new home portifolio

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,40 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Portfólio de Fabio',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green, // Cor primária verde
+        scaffoldBackgroundColor: Colors.black, // Fundo preto
+        textTheme: TextTheme(
+          headlineLarge: TextStyle(
+            // Substitua headline1 por headlineLarge
+            fontSize: 36,
+            fontWeight: FontWeight.bold,
+            color: Colors.green, // Texto verde
+          ),
+          bodyLarge: TextStyle(
+            // Substitua bodyText1 por bodyLarge
+            fontSize: 18,
+            color: Colors.greenAccent, // Texto verde claro
+          ),
+          labelLarge: TextStyle(
+            // Substitua button por labelLarge
+            fontSize: 16,
+            color: Colors.black, // Texto preto para botões
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          color: Colors.black, // Fundo preto para a AppBar
+          titleTextStyle: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.green, // Texto verde para o título da AppBar
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.green, // Cor de fundo dos botões
+            foregroundColor: Colors.black, // Texto preto nos botões
+          ),
+        ),
       ),
       home: HomePage(), // Defina a HomePage como a página inicial
     );
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -4,146 +4,47 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Portfólio de Fabio'),
+      ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            // Seção 1: Home
-            Container(
-              height: MediaQuery.of(context).size.height,
-              padding: EdgeInsets.all(20),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Olá, eu sou Fabio!',
-                      style:
-                          TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      'Desenvolvedor Flutter | Full Stack Enthusiast',
-                      style: TextStyle(fontSize: 18, color: Colors.grey),
-                    ),
-                    SizedBox(height: 40),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Rola para a próxima seção
-                        Scrollable.ensureVisible(
-                          context,
-                          duration: Duration(seconds: 1),
-                          curve: Curves.easeInOut,
-                        );
-                      },
-                      child: Text('Saiba Mais'),
-                    ),
-                  ],
+        // Adiciona a capacidade de rolar
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Olá, eu sou Fabio!',
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Desenvolvedor Flutter | Full Stack Enthusiast',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {
+                  // Navegar para a próxima seção
+                },
+                child: Text(
+                  'Saiba Mais',
+                  style: Theme.of(context).textTheme.labelLarge,
                 ),
               ),
-            ),
-
-            // Seção 2: Tecnologias
-            Container(
-              height: MediaQuery.of(context).size.height,
-              padding: EdgeInsets.all(20),
-              color: Colors.grey[200],
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Tecnologias que Trabalho',
-                      style:
-                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 20),
-                    Wrap(
-                      spacing: 10,
-                      runSpacing: 10,
-                      children: [
-                        Chip(label: Text('Flutter')),
-                        Chip(label: Text('Dart')),
-                        Chip(label: Text('Firebase')),
-                        Chip(label: Text('React')),
-                        Chip(label: Text('TypeScript')),
-                      ],
-                    ),
-                  ],
+              // Adicione mais conteúdo aqui para testar a rolagem
+              Container(
+                height: 1000, // Altura grande para forçar a rolagem
+                color: Colors.green.withOpacity(0.1),
+                child: Center(
+                  child: Text(
+                    'Mais conteúdo abaixo...',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
                 ),
               ),
-            ),
-
-            // Seção 3: Projetos
-            Container(
-              height: MediaQuery.of(context).size.height,
-              padding: EdgeInsets.all(20),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Projetos Selecionados',
-                      style:
-                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 20),
-                    // Exemplo de card de projeto
-                    Card(
-                      child: Padding(
-                        padding: EdgeInsets.all(16),
-                        child: Column(
-                          children: [
-                            Text(
-                              'Ecommerce Website',
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(height: 10),
-                            Text(
-                              'Descrição breve do projeto.',
-                              style: TextStyle(fontSize: 16),
-                            ),
-                            SizedBox(height: 10),
-                            ElevatedButton(
-                              onPressed: () {
-                                // Link para o projeto
-                              },
-                              child: Text('Ver Projeto'),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            // Seção 4: Contato
-            Container(
-              height: MediaQuery.of(context).size.height,
-              padding: EdgeInsets.all(20),
-              color: Colors.grey[200],
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Entre em Contato',
-                      style:
-                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 20),
-                    Text('Email: p.sonats@gmail.com'),
-                    SizedBox(height: 10),
-                    Text('LinkedIn: [Seu LinkedIn]'),
-                    SizedBox(height: 10),
-                    Text('GitHub: [Seu GitHub]'),
-                  ],
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
